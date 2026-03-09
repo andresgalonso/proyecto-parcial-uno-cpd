ba2cf89c7641034968327cf1aa89162fe7a0f715
Andres <andres.gonzalez.alonso10@gmail.com>
2026-03-08 19:09:00 -0600

    Actualizacion de locaciones

--- login.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById('togglePass');
-const pass = document.getElementById('passInput');
-
-toggle.addEventListener('click', () => {
-    const isText = pass.type === 'text';
-    pass.type = isText ? 'password' : 'text';
-    toggle.textContent = isText ? 'visibility' : 'visibility_off';
-});
